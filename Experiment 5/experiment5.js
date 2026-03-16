7cad4a9cb36d9d9f4e83cab3c71b1496304669ca
email <vishalsenta9@gmail.com>
2026-03-16 12:18:15 +0530

    text change accomplished

--- Experiment 5/experiment5.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("button1");
-const bg = document.getElementsByTagName("body")
-button.addEventListener('click', function() {
-    button.style.backgroundColor = 'black';
-    button.style.color = 'white';
-    bg.style.backgroundColor = 'black';
-    
-
-})
